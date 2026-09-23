904ceb701eb526157d15f1e2076e5037265a326f
Winfred Nkatha <nkathawinnie94@gmail.com>
2026-09-23 21:19:22 +0300

    feat(server): register M-Pesa and order routes in server.js

--- server/server.js
+++ server/server.js
@@ -24,6 +24,8 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/testimonials', require('./routes/testimonials'));
 app.use('/api/cart', require('./routes/cart'));
+app.use('/api/mpesa', require('./routes/mpesa'));
+app.use('/api/orders', require('./routes/orders'));
 
 // Basic route
 app.get('/', (req, res) => {
