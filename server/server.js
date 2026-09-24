9c153569c83c31f4e5242ce0c1bc8f6257923958
Winfred Nkatha <nkathawinnie94@gmail.com>
2026-09-24 23:38:15 +0300

    refactor(server): mount Paystack routes in place of M-Pesa

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/testimonials', require('./routes/testimonials'));
 app.use('/api/cart', require('./routes/cart'));
-app.use('/api/mpesa', require('./routes/mpesa'));
+app.use('/api/paystack', require('./routes/paystack'));
 app.use('/api/orders', require('./routes/orders'));
 
 // Basic route
